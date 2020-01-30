60c88f4633c562046969ef1b221698a77ed963f8
Esteban <elcelibus@gmail.com>
2020-01-30 15:54:38 +0100

    subir sitio

---
+++ precache-manifest.b454b490b32ceca4a31a213c6fd26e71.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1febce69bbc93d4c9b39ee09ab74107",
+    "url": "/estebansanmartin/js.git/index.html"
+  },
+  {
+    "revision": "4e1b25b96e588afba403",
+    "url": "/estebansanmartin/js.git/static/css/main.1c1865cc.chunk.css"
+  },
+  {
+    "revision": "3bd560ea92aea598f802",
+    "url": "/estebansanmartin/js.git/static/js/2.579f7820.chunk.js"
+  },
+  {
+    "revision": "4e1b25b96e588afba403",
+    "url": "/estebansanmartin/js.git/static/js/main.2dbdf13b.chunk.js"
+  },
+  {
+    "revision": "97f388e3ef868ee93191",
+    "url": "/estebansanmartin/js.git/static/js/runtime~main.74eb5ee6.js"
+  },
+  {
+    "revision": "02fcc056019a219fd47eb7065ab54749",
+    "url": "/estebansanmartin/js.git/static/media/assistenza.02fcc056.jpg"
+  },
+  {
+    "revision": "a0cc521281dbf8017b450d6ea7837e5c",
+    "url": "/estebansanmartin/js.git/static/media/constructora.a0cc5212.png"
+  },
+  {
+    "revision": "0361100ea2752890342e70c4b9f4827d",
+    "url": "/estebansanmartin/js.git/static/media/edilizia.0361100e.jpg"
+  },
+  {
+    "revision": "448d283db0674d5a33cb47cbeba6b371",
+    "url": "/estebansanmartin/js.git/static/media/logo3d.448d283d.png"
+  },
+  {
+    "revision": "a15e78a6d71287bda80eed2ce8b133d9",
+    "url": "/estebansanmartin/js.git/static/media/portfolio.a15e78a6.jpg"
+  },
+  {
+    "revision": "3d956045a355f96ad9d745f560187e34",
+    "url": "/estebansanmartin/js.git/static/media/restaurant.3d956045.jpg"
+  },
+  {
+    "revision": "9140fba332c80adfa895415a54c09ddc",
+    "url": "/estebansanmartin/js.git/static/media/tips.9140fba3.jpg"
+  },
+  {
+    "revision": "0193b683560e2444f27da932ed438bac",
+    "url": "/estebansanmartin/js.git/static/media/torino.0193b683.jpg"
+  }
+]);
